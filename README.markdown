7351c45779960adedc373a87d4521600a2625654
windowshady <rriggs@gmail.com>
2012-02-14 08:43:05 -0500

    Improved the README file

---
+++ README.markdown
@@ -0,0 +1,4 @@
+# Ruby on Rails Tutorial: first application
+
+This file is the first application for
+[*Ruby on Rails Tutorial: Learn Rails by Example*]
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = 'ba2068e9ff752bde5e5d454f018e8be95e8fa0abd932a9aa06354d28c44ca9975258fc3c0235e735d2fb8a192fb0581ec665eb51e72c1f2a5e6b50ec6b1ca6a7'
